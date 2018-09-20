eb9d9d40f6ca19cd333ca8e358c3d2323c945fad
Yun <a@a.com>
2018-09-20 22:21:56 +1000

    Add projects

---
+++ Reverse Words in a String II/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Words_in_a_String_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = new char[] { 't', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e' };
+            ReverseWords(chars);
+            
+
+        }
+
+        static public void ReverseWords(char[] str)
+        {
+            Reverse(str, 0, str.Length - 1);
+
+            int start = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(str[i] == ' ')
+                {
+                    Reverse(str, start, i - 1);
+                    start = i + 1;
+                }
+            }
+
+            Reverse(str, start, str.Length - 1);
+
+        }
+
+        private static void Reverse(char[] str, int startPos, int endPos)
+        {
+            while(startPos<endPos)
+            {
+                char temp = str[startPos];
+                str[startPos] = str[endPos];
+                str[endPos] = temp;
+                startPos++;
+                endPos--;
+            }
+
+        }
+    }
+}
---
+++ Two Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbs = new int[] { 3,2,4};
+            var result = TwoSum(numbs, 6);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            System.Collections.Hashtable hash = new System.Collections.Hashtable();
+            for(int i=0;i<nums.Length;i++)
+            {
+                int element = nums[i];
+                int diff = target - element;
+
+                if(hash.ContainsKey(diff) )
+                {
+                    return new int[2] { i, Convert.ToInt32(hash[diff]) };
+                }
+
+                hash.Add(element, i);
+
+            }
+
+            throw new InvalidProgramException();
+
+
+
+
+        }
+    }
+}

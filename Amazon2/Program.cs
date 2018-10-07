0d0f3083f8651be1a713d89f1052761f3122200e
Yun <a@a.com>
2018-10-07 23:32:05 +1100

    ..

--- Amazon2/Program.cs
+++ Amazon2/Program.cs
@@ -10,7 +10,8 @@ namespace Amazon2
     {
         static void Main(string[] args)
         {
-            int[,] area = new[,] { { 1, 0, 0 }, { 1, 0, 0 }, { 1, 9, 1 } };
+            //int[,] area = new[,] { { 1, 0, 0 }, { 1, 0, 0 }, { 1, 9, 1 } };
+            int[,] area = new[,] { { 1, 1, 1 }, { 0, 0, 1 }, { 1, 9, 1 } };
             minimumDistance(3, 3, area);
         }
 
@@ -21,21 +22,22 @@ namespace Amazon2
             int nr = numRows;
 
             int nc = numColumns;
-            Dfs(area, 0, 0);
+            Dfs(area, 0, 0, 0);
            
 
-            Console.WriteLine(a);
+            //Console.WriteLine(a);
             Console.WriteLine(final);
             return numberOfRoute;
         }
 
-        private static int a;
+        //private static int a;
         private static int final;
 
-        private static void Dfs(int[,] grid, int r, int c)
+        private static void Dfs(int[,] grid, int r, int c, int routePast)
         {
             int nr = grid.GetLength(0);
             int nc = grid.GetLength(1);
+            
 
             
             if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r, c] == 0)
@@ -45,18 +47,19 @@ namespace Amazon2
 
             if(grid[r, c] == 9)
             {
-                final = a;
+                final = routePast;
+                return;
             }
 
             if (grid[r, c] == 1)
-                a++;
+                routePast++;
 
 
             grid[r, c] = 0;
-            Dfs(grid, r - 1, c);
-            Dfs(grid, r + 1, c);
-            Dfs(grid, r, c + 1);
-            Dfs(grid, r, c - 1);
+            Dfs(grid, r - 1, c, routePast);
+            Dfs(grid, r + 1, c, routePast);
+            Dfs(grid, r, c + 1, routePast);
+            Dfs(grid, r, c - 1, routePast);
         }
     }
 }
---
+++ Count Univalue Subtrees/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Univalue_Subtrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var five = new TreeNode(5);
+            var one = new TreeNode(1);
+            var five2 = new TreeNode(5);
+            var five3 = new TreeNode(5);
+            var five4 = new TreeNode(5);
+            var five5 = new TreeNode(5);
+
+            five.left = one;
+            five.right = five2;
+            one.left = five3;
+            one.right = five4;
+            five2.right = five5;
+
+            Console.WriteLine(new Solution().CountUnivalSubtrees(five));
+
+        }
+    }
+
+    public class Solution
+    {
+        public int CountUnivalSubtrees(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var result = CheckSubTree(root, root.val) ? 1 : 0;
+
+            return result + CountUnivalSubtrees(root.left) + CountUnivalSubtrees(root.right);
+        }
+
+        private bool CheckSubTree(TreeNode node, int val )
+        {
+            if (node == null) return true;
+
+            return node.val == val && CheckSubTree(node.left, node.val) && CheckSubTree(node.right, node.val);
+
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}

7f198e610c8769bc21f4b20d06d721892252703f
Yunkwan Kim <neowhaleeye@gmail.com>
2019-06-24 22:49:10 +1000

    ,,,

---
+++ Letter Combinations of a Phone Number/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letter_Combinations_of_a_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var v = new Solution().LetterCombinations("2345");
+            v.ToList().ForEach(e => Console.WriteLine(e));
+        }
+    }
+
+    public class Solution
+    {
+
+        private readonly Dictionary<char, char[]> dic = new Dictionary<char, char[]>();
+
+        public Solution()
+        {
+            dic.Add('2', new char[] { 'a', 'b', 'c' });
+            dic.Add('3', new char[] { 'd', 'e', 'f' });
+            dic.Add('4', new char[] { 'g', 'h', 'i' });
+            dic.Add('5', new char[] { 'j', 'k', 'l' });
+            dic.Add('6', new char[] { 'm', 'n', 'o' });
+            dic.Add('7', new char[] { 'p', 'q', 'r', 's' });
+            dic.Add('8', new char[] { 't', 'u', 'v' });
+            dic.Add('9', new char[] { 'w', 'x', 'y', 'z' });
+        }
+
+
+
+
+
+
+        public IList<string> LetterCombinations(string digits)
+        {
+            IList<string> curr = new List<string>();
+            if (digits.Length == 0) return curr;
+
+            curr.Add("");
+            foreach (char d in digits)
+            {
+                IList<string> next = new List<string>();
+                foreach (char letter in dic[d])
+                {
+                    foreach (string s in curr)
+                    {
+                        next.Add(s + letter);
+                    }
+                }
+                curr = next;
+            }
+            return curr;
+
+
+
+
+        }
+
+        
+
+          
+    }
+
+}
---
+++ Median of Two Sorted Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Median_of_Two_Sorted_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> hs = new List<int>();
+
+            int[] nums1 = new int[] { 1,1};
+            int[] nums2 = new int[] { 1,2};
+
+            foreach (int n in nums1)
+            {
+                hs.Add(n);
+            }
+            foreach (int n in nums2)
+            {
+                hs.Add(n);
+            }
+
+            int median = hs.Count / 2;
+            int r = hs.Count % 2;
+
+            int startPnt = median, endPnt = median;
+            IEnumerable<int> result = null;
+            double sum = 0;
+            if (r == 0)
+            {
+                startPnt = median - 1;
+                result = hs.OrderBy(e => e).Skip(startPnt).Take(2);
+                sum = Convert.ToDouble(result.Sum()) / 2;
+            }
+            else
+            {
+                result = hs.OrderBy(e => e).Skip(startPnt).Take(1);
+                sum = result.Sum();
+            }
+
+
+            
+
+        }
+    }
+}
